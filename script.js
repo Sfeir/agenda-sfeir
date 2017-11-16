be72118e6c3636833255a499c62362bcaf9be4d7
vplouy <vplouy@gmail.com>
2017-11-16 12:06:25 +0100

    google script v1.6

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+
+
+function showInfo(){
+    $('#organisateur').removeClass('hidden');
+    $('#speaker').removeClass('hidden');
+    $('#description').removeClass('hidden');
+    $('#lien').removeClass('hidden');
+    $('#email').removeClass('hidden');
+    $('#postForm').prop('disabled', false);
+}
+
+function hideInfo(){
+    $('#organisateur').addClass('hidden');
+    $('#speaker').addClass('hidden');
+    $('#description').addClass('hidden');
+    $('#lien').addClass('hidden');
+    $('#email').addClass('hidden');
+    $('#postForm').prop('disabled', true);
+
+}
+
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '610580318952-gbttqu242bfi93erapqcvcqtb8ot0l3n.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyAZJHOwagBVkC6GXSFqn2G9jN6D2sIGNRE';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        //listMajors();
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
 $().ready(function() {
     handleAuthClick();
     $('#contact-form').bootstrapValidator({
@@ -227,50 +319,50 @@ $().ready(function() {
 
                     //if($('#cv').val() !== "Autre" && $('#jour2').is(':checked')){
 
-                        if($('#ph').val() !== "Soir" && $('#jour2').is(':checked') && row[4] === $('#cs').val()){
-                            var from = Date.parse(row[6]);
-                            var to   = Date.parse(row[7]);
-                            var check = Date.parse($('#dd').val());
+                    if($('#ph').val() !== "Soir" && $('#jour2').is(':checked') && row[4] === $('#cs').val()){
+                        var from = Date.parse(row[6]);
+                        var to   = Date.parse(row[7]);
+                        var check = Date.parse($('#dd').val());
 
-                            if(check <= to && check >= from){
-                                $('#invalide1').removeClass('hidden');
-                                hideInfo();
-                                break;
-                            }else{
-                                $('#invalide1').addClass('hidden');
-                                showInfo();
-                            }}//else*/
-                         if(row[6] === $('#dd').val() && row[4] === $('#cs').val()){
-                            if($('#ph').val() === "Matin"){
-                                if(row[5] === "Toute la journée" || row[5] === "Matin"){
-                                    $('#invalide1').removeClass('hidden');
-                                    hideInfo();
-                                    break;
-                                }
-                            }
-                            else if($('#ph').val() === "Après-midi"){
-                                if(row[5] === "Toute la journée" || row[5] === "Après-midi"){
-                                    $('#invalide1').removeClass('hidden');
-                                    hideInfo();
-                                    break;
-                                }
-                            }
-                            else if($('#ph').val() === "Toute la journée"){
-                                if(row[5] === "Toute la journée" || row[5] === "Après-midi" || row[5] === "Matin"){
-                                    $('#invalide1').removeClass('hidden');
-                                    hideInfo();
-                                    break;
-                                }
-                            }
-                            else if($('#ph').val() === "Soir" && row[5] === "Soir"){
-                                    $('#invalide1').removeClass('hidden');
-                                    hideInfo();
-                                    break;
-                            }
+                        if(check <= to && check >= from){
+                            $('#invalide1').removeClass('hidden');
+                            hideInfo();
+                            break;
                         }else{
                             $('#invalide1').addClass('hidden');
                             showInfo();
+                        }}//else*/
+                    if(row[6] === $('#dd').val() && row[4] === $('#cs').val()){
+                        if($('#ph').val() === "Matin"){
+                            if(row[5] === "Toute la journée" || row[5] === "Matin"){
+                                $('#invalide1').removeClass('hidden');
+                                hideInfo();
+                                break;
+                            }
                         }
+                        else if($('#ph').val() === "Après-midi"){
+                            if(row[5] === "Toute la journée" || row[5] === "Après-midi"){
+                                $('#invalide1').removeClass('hidden');
+                                hideInfo();
+                                break;
+                            }
+                        }
+                        else if($('#ph').val() === "Toute la journée"){
+                            if(row[5] === "Toute la journée" || row[5] === "Après-midi" || row[5] === "Matin"){
+                                $('#invalide1').removeClass('hidden');
+                                hideInfo();
+                                break;
+                            }
+                        }
+                        else if($('#ph').val() === "Soir" && row[5] === "Soir"){
+                            $('#invalide1').removeClass('hidden');
+                            hideInfo();
+                            break;
+                        }
+                    }else{
+                        $('#invalide1').addClass('hidden');
+                        showInfo();
+                    }
                     //}
                 }
             } else {
@@ -313,93 +405,3 @@ $().ready(function() {
     });
 
 });
-
-function showInfo(){
-    $('#organisateur').removeClass('hidden');
-    $('#speaker').removeClass('hidden');
-    $('#description').removeClass('hidden');
-    $('#lien').removeClass('hidden');
-    $('#email').removeClass('hidden');
-    $('#postForm').prop('disabled', false);
-}
-
-function hideInfo(){
-    $('#organisateur').addClass('hidden');
-    $('#speaker').addClass('hidden');
-    $('#description').addClass('hidden');
-    $('#lien').addClass('hidden');
-    $('#email').addClass('hidden');
-    $('#postForm').prop('disabled', true);
-
-}
-
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '610580318952-gbttqu242bfi93erapqcvcqtb8ot0l3n.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyAZJHOwagBVkC6GXSFqn2G9jN6D2sIGNRE';
-
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
-
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-    }).then(function () {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
-    });
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-function updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-        authorizeButton.style.display = 'none';
-        signoutButton.style.display = 'block';
-        //listMajors();
-    } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
-    }
-}
-
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick() {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-/**
- *  Sign out the user upon button click.
- */
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
